b302f231435769980c57fd123ac448edc7fc3bdd
pedro sanchez <pedrosanchez707@gmail.com>
2022-06-12 23:15:23 -0700

    Insert data into table

--- Database.sql
+++ Database.sql
@@ -2,9 +2,14 @@ CREATE DATABASE IF NOT EXISTS USER;
 
 USE USER;
 
-CREATE TABLE user(
+CREATE TABLE IF NOT EXISTS user(
     id INT PRIMARY KEY AUTO_INCREMENT,
     first_name varchar(255),
     last_name varchar(255)
-)
+);
 
+INSERT INTO user 
+VALUES
+(1,'John','Smith'),
+(2,'Lucky','Smith'),
+(3,'APPLE','Smith');
